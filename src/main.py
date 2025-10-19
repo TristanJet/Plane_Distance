4adcf7756591f24808534fb1506d7ee574df5a89
Tristan Jet <tristanjetlay1@gmail.com>
2025-10-20 00:50:23 +0300

    edits to point system and wrapper

--- src/main.py
+++ src/main.py
@@ -8,30 +8,35 @@ def main():
     min = -180
     max = min + intv
     db.connect()
-    countries = db.getCountriesInRange(min, max)
-    airports = db.getAirports(random.choice(countries), min, max)
+    airports = getAirportsLong(min, max)
     ap = [airportFromRow(random.choice(airports)), None]
     while max != 180:
         min += intv
         max += intv
-        countries = db.getCountriesInRange(min, max)
-        airports = db.getAirports(random.choice(countries), min, max)
+        airports = getAirportsLong(min, max)
         ap[1] = airportFromRow(random.choice(airports))
-        distance = haversine((ap[0]['lat'], ap[0]['long']), (ap[1]['lat'], ap[1]['long']), unit=Unit.KILOMETERS)
+        distance = int(haversine((ap[0]['lat'], ap[0]['long']), (ap[1]['lat'], ap[1]['long']), unit=Unit.KILOMETERS))
         print(
             f"What is the distance between:\n{ap[0]['name']} - {ap[0]['munic']}, {ap[0]['country']}\nand\n{ap[1]['name']} - {ap[1]['munic']}, {ap[1]['country']}"
         )
-        in_dist = float(input(':'))
-        print(f"You're guess was {in_dist}\nThe actual distance was: {distance}")
+        in_dist = int(input(':'))
+        print(f"You're guess was {in_dist}\nThe actual distance was: {distance} KM")
         diff = abs(distance - in_dist)
         print(f"You were off by: {diff} KM")
-        if (diff) <= 500:
+        if diff <= 500:
+            score += 2
+            print(f"You get two points!")
+        elif diff <= 1000:
             score += 1
             print(f"You get one point!")
         ap[0] = ap[1]
         print("------------------")
     print(f"You're final score is {score}")
     db.close()
+
+def getAirportsLong(min, max):
+    countries = db.getCountriesInRange(min, max)
+    return db.getAirports(random.choice(countries), min, max)
 
 def airportFromRow(row):
     return {
